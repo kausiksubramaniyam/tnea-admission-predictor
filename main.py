74a0668ea2001f1930b7fc2fc7a2ca1a61fa72ab
Kausik Subramaniyam G <59435311+kausiksubramaniyam@users.noreply.github.com>
2022-06-03 23:06:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pickle
 import json
-import pandas as pd
-import numpy as np
 from sklearn import linear_model
 from sklearn.ensemble import RandomForestRegressor
 loaded_model = pickle.load(open('finalized_model2.sav', 'rb'))
