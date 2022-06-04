02d4f1fd07878a32cc600f3edd7ddc13ac1a7e8e
Kausik Subramaniyam G <59435311+kausiksubramaniyam@users.noreply.github.com>
2022-06-04 12:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pickle
 import json
 from sklearn import linear_model
 from sklearn.ensemble import RandomForestRegressor
-loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
+loaded_model = pickle.load(open('finalized_model2.sav', 'rb'))
 def predict_cutoff(a):
 	clg=loaded_model.predict([a])
 	return clg
